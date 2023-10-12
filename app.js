870fef5e5e405ea3c2e3ed4e9b2cda614c1e889f
yuhanz1 <yuhanz9149@gmail.com>
2023-10-12 09:49:45 -0400

    2

--- app.js
+++ app.js
@@ -2,38 +2,41 @@ const constellations=[
     {
         name: "secretivename",
         supporterofdokja: true,
-        qualities: ["Secretive Plotter", "more than one", "the most ancient dream"],
+        qualities: ["Secretive Plotter", "more than one", "the most ancient dream", "1683", "outer"],
     },
     {
         name: "Persephone",
         supporterofdokja:true,
-        qualities: ["Queen of the Darkest Spring", "kind", "dried up",],
+        qualities: ["Queen of the Darkest Spring", "kind", "dried up","queen", "mother figure"],
     },
     {
         name: "extremelylongnamethatwasneverrevealed",
         supporterofdokja: true,
-        qualities: ["Black Abyssal Dragon", "kindhearted",],
+        qualities: ["Black Abyssal Dragon", "kindhearted", "chuuni", "kindbutdoesntshow", "cute"],
     },
     {
         name: "Sun Wukong",
         supporterofdokja: true,
-        qualities: ["Prisoner of the Golden Headband", "golden hair"],
+        qualities: ["Prisoner of the Golden Headband", "golden hair", "receiving end of dokja's admiration", "monkey", "proud"],
     },
     {
         name: "???",
         supportofdokja: false,
-        qualities: ["Most Ancient Dream", "who?",],
+        qualities: ["Most Ancient Dream", "who?",  "?", "??", "???", ":(("],
     },
     {
         name: "---",
         supporterofdokja: true,
-        qualities: ["Demon King of Salvation", "weirdo", "epilogue", "eternity"],
+        qualities: ["Demon King of Salvation", "weirdo", "epilogue", "eternity", "dontsayyourdreamoutloudoritllnotcometrueidiot"],
     },
 ];
 
 constellations.forEach((el)=> console.log(el.name));
-constellations.forEach((el)=> console.log(el.qualities));
-
+constellations.forEach((el)=> console.log(el.qualities[0]));
+constellations.forEach((el)=> console.log(el.qualities[1]));
+constellations.forEach((el)=> console.log(el.qualities[2]));
+constellations.forEach((el)=> console.log(el.qualities[3]));
+constellations.forEach((el)=> console.log(el.qualities[4]));
 
 
 const supporter = constellations.filter((constellations)=> constellations.supporterofdokja === true);
